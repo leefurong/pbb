7f67bb5b76a9ba973fc61086f2f6871f8bf07e3a
李富荣 <leefurong@gmail.com>
2021-12-20 16:48:29 +0800

    敌船炸弹只有在碰到飞机才爆炸。

--- ai/boat_bomb.py
+++ ai/boat_bomb.py
@@ -5,8 +5,10 @@ class BoatBomb(Bomb):
     def update(self):
         if self.face == 0:
             self.forward(5)
-        if self.rect.y<=10 and self.face == 0:
+        if self.face == 1 and self.baozha_time + 3 <= time.time():
+            self.dead = True
+        if self.ai.pengdao(self, Plane) and self.face == 0:
             self.next_face()
             self.baozha_time = time.time()
-        if self.face == 1 and self.baozha_time + 3 <= time.time():
+        if self.rect.y < 10:
             self.dead = True
