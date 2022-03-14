33a811d487bed352bc11525aae8d126f6108e72b
李富荣 <leefurong@gmail.com>
2022-03-14 16:59:05 +0800

    冰娃与火娃： 框架

---
+++ fire_and_ice/game.py
@@ -0,0 +1,26 @@
+from ice import Ice
+import pygame, sys
+
+class Game:
+    def __init__(self):
+        self.screen = pygame.display.set_mode(
+            (800, 800))
+        self.actors = {"ice": Ice()}
+
+    def handleEvents(self):
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.screen.fill((230, 230, 230))
+        pygame.display.flip()
+
+    def run(self):
+        while True:
+            self.handleEvents()
+            self.update()
+            self.draw()
---
+++ fire_and_ice/ice.py
@@ -0,0 +1,2 @@
+class Ice:
+    pass
