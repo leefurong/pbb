bab7f2fe5506c25faca45f054d19c9db57dc1b3d
李富荣 <leefurong@gmail.com>
2022-06-27 17:46:39 +0800

    正在实现棋盘运动算法。 实现了movemovemove函数，用来计算某个棋子可以最远移动到哪里。

---
+++ 2048/manager.py
@@ -0,0 +1,15 @@
+from qipan import qipan
+class manager:
+    def __init__(self):
+        self.q = qipan()
+        pass
+    def up(self):
+        for i in range(self.q.size):
+            for j in range(1, self.q.size):
+                pass
+    def down(self):
+        pass
+    def left(self):
+        pass
+    def right(self):
+        pass
--- 2048/qipan.py
+++ 2048/qipan.py
@@ -1,8 +1,17 @@
+dlie=[0,0,-1,1]
+dhang=[-1,1,0,0]
+
+class direction:
+    up=0
+    down=1
+    left=2
+    right=3
 class qipan:
     def __init__(self, size):
         self.size=size
         self.gezi = [[0]*size for _ in range(size)]
-
+    def inside(self , hang, lie):
+        return 0<=hang<self.size and self.size>lie>=0 
     def hasMate(self , hang, lie):
         sum=self.gezi[hang][lie]
         if hang<self.size-1 and self.gezi[hang+1][lie]==sum:return True
@@ -23,18 +32,30 @@ class qipan:
         return False
 
     def canMoveTo(self,v,hang2, lie2):
-        return self.gezi[hang2][lie2]==0 or self.gezi[hang2][lie2]==v
+        return self.inside(hang2, lie2) and self.gezi[hang2][lie2]==0 or self.gezi[hang2][lie2]==v
+
+    def movemovemove(self, hang, lie, direction):
+        me = self.what(hang, lie)
+        newhang = hang+dhang[direction]
+        newlie = lie+dlie[direction]
+        while self.inside(newhang, newlie) and self.what(newhang, newlie) == 0:
+            newhang  += dhang[direction]
+            newlie  += dlie[direction]
+        if self.inside(newhang, newlie) and me == self.what(newhang, newlie):
+            return newhang,newlie
+        else:
+            return newhang - dhang[direction], newlie - dlie[direction]
 
     def what(self, hang, lie):
         return self.gezi[hang][lie]
 
 if __name__=="__main__":
-    a = qipan(2)
-    a.set(0, 1, 26)
-    a.set(1, 1, 2048)
-    a.set(1, 0, 12)
-    a.set(0, 0, 2048)
-    print(a.isAlive())
+    a = qipan(4)
+    a.set(0, 0, 2)
+    a.set(0, 1, 4)
+    a.set(0, 2, 0)
+    a.set(0, 3, 4)
+    print(a.movemovemove(0, 0, direction.right))
 
     # b = qipan(8)
     # b.print()
